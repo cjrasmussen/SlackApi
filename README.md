b9656460894134b628a40b7669609b2b031e64d3
Clark Rasmussen <cjr@clarkrasmussen.com>
2020-09-14 17:05:29 -0400

    Initial commit, finally composerizing this

---
+++ README.md
@@ -0,0 +1,44 @@
+# SlackApi
+
+Simple class for making requests to the Slack API or Slack Webhooks.  Not affiliated with Slack.
+
+
+## Usage
+
+```php
+use cjrasmussen\SlackApi;
+
+// INVITE A USER TO A CHANNEL
+$slack = new SlackApi($token);
+
+$args = [
+    'channel' => 'C1234567',
+    'member' => 'U9876543',
+];
+$slack->request('POST', 'conversations.invite', $args);
+
+// SEND A MESSAGE VIA A SLACK WEBHOOK
+$msg = [
+    'text' => 'Message text',
+];
+$response = (new SlackApi($webhook_url))->sendMessage($msg);
+```
+
+## More Examples
+
+More examples, as well as other things I've learned using the Slack API, are [available at my blog](https://blog.cjr.dev/?s=Slack).
+
+## Installation
+
+Simply add a dependency on cjrasmussen/slack-api to your composer.json file if you use [Composer](https://getcomposer.org/) to manage the dependencies of your project:
+
+```sh
+composer require cjrasmussen/slack-api
+```
+
+Although it's recommended to use Composer, you can actually include the file(s) any way you want.
+
+
+## License
+
+SlackApi is [MIT](http://opensource.org/licenses/MIT) licensed.
---
+++ src/SlackApi.php
@@ -0,0 +1,113 @@
+<?php
+namespace cjrasmussen\SlackApi;
+
+use RuntimeException;
+
+/**
+ * Class Slack
+ * @package O6
+ */
+class SlackApi
+{
+	private $token;
+	private $api_url = 'https://slack.com/api/';
+	private $webhook_url;
+
+	/**
+	 * Slack constructor.
+	 *
+	 * @param string $mixed - Slack token or webhook URL
+	 * @param string|null $team - Slack team URL part
+	 * @param string|null $webhook_url - Slack webhook URL if not provided as $mixed
+	 */
+	public function __construct($mixed, $team = null, $webhook_url = null)
+	{
+		if ((false !== strpos($mixed, 'hooks.slack')) AND ($webhook_url === null)) {
+			$this->token = null;
+			$this->webhook_url = $mixed;
+		} else {
+			$this->token = $mixed;
+			$this->webhook_url = $webhook_url;
+		}
+
+		if ($team) {
+			$this->api_url = 'https://' . $team . '.slack.com/api/';
+		}
+	}
+
+	/**
+	 * Make a request to the Slack API
+	 *
+	 * @param string $type
+	 * @param string $method
+	 * @param array|null $args
+	 * @return mixed
+	 */
+	public function request($type, $method, ?array $args = [])
+	{
+		$url = $this->api_url . $method;
+		$args['token'] = $this->token;
+
+		if (($type === 'GET') AND (count($args))) {
+			$url .= '?' . http_build_query($args);
+		}
+
+		$c = curl_init();
+		curl_setopt($c, CURLOPT_URL, $url);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+		switch ($type) {
+			case 'POST':
+				curl_setopt($c, CURLOPT_POST, 1);
+				curl_setopt($c, CURLOPT_POSTFIELDS, $args);
+				break;
+			case 'GET':
+				curl_setopt($c, CURLOPT_HTTPGET, 1);
+				break;
+			default:
+				curl_setopt($c, CURLOPT_CUSTOMREQUEST, $type);
+		}
+
+		$response = curl_exec($c);
+		curl_close($c);
+
+		$data = json_decode($response);
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			throw new RuntimeException('API response was not valid JSON');
+		}
+
+		if ($data->error) {
+			throw new RuntimeException($data->error);
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Send a message to Slack via a webhook
+	 *
+	 * @param string|array $data
+	 * @return bool|string
+	 */
+	public function sendMessage($data)
+	{
+		$msg = [];
+		if (is_string($data)) {
+			$msg['text'] = $data;
+		} else {
+			$msg = $data;
+		}
+
+		$c = curl_init();
+		curl_setopt($c, CURLOPT_URL, $this->webhook_url);
+		curl_setopt($c, CURLOPT_POST, 1);
+		curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($msg));
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($c, CURLOPT_HEADER, 0);
+		curl_setopt($c, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
+		$success = curl_exec($c);
+		curl_close($c);
+
+		return $success;
+	}
+}
